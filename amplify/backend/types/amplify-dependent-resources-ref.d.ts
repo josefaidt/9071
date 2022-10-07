a988fc7de061512947b63f220013211bb9dbe030
josefaidt <josef.aidt@gmail.com>
2022-10-07 11:31:30 -0500

    🚀

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,12 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "function": {
-        "90717fda076a": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "9071aaaf2cf4": {
+        "907172f2662a": {
             "Name": "string",
             "Arn": "string",
             "Region": "string",
